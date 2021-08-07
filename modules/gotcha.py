00000a6b080d592af460ab3f5c1dad4cd297d3fa
d3fau1t <aciddust20@gmail.com>
2021-08-07 23:37:33 +0900

    Update, 동전던지기 확률 반반

--- modules/gotcha.py
+++ modules/gotcha.py
@@ -2,10 +2,8 @@ from random import randint
 
 
 async def get_result() -> str:
-    result = "한번 더 해봐"
-    number = randint(0, 2)
+    result = "앞"
+    number = randint(0, 1)
     if number == 0:
-        result = "운이 없군. 꽝이야"
-    elif number == 1:
-        result = "당첨!"
+        result = "뒤"
     return result
