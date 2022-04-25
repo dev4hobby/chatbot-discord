532f5d9ce669e8ebfa839f2435c71c69b45064b1
Anthony <aciddust20@gmail.com>
2022-04-26 00:34:03 +0900

    chore: test commit

--- modules/weather.py
+++ modules/weather.py
@@ -5,22 +5,28 @@ import urllib
 
 async def check_weather(message):
     weather = message.content.split()
+    static_weather = urllib.parse.quote("날씨")
+
     if len(weather) > 1:
-        enc_location = urllib.parse.quote(f"{weather[1]}+날씨")
+        enc_location = urllib.parse.quote(f"{weather[1]}") + f"+{static_weather}" 
     else:
-        enc_location = urllib.parse.quote("강남+날씨")
+        enc_location = urllib.parse.quote(f"강남구") + f"+{static_weather}"
 
     try:
-        url = f"https://search.naver.com/search.naver?id=utf8&query={enc_location}"
+        url = f"https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query={enc_location}&oquery={static_weather}&tqi=hEj1pwp0JXoss7YFECCssssssfw-143894"
+        print(url)
         req = Request(url)
         page = urlopen(req)
         html = page.read()
         soup = bs4.BeautifulSoup(html, "html5lib")
 
-        response = f"""현재 {soup.find('span', class_='btn_select').find('em').text}의 기온은 {soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text}도 입니다.
-{soup.find('p', class_='cast_txt').text}"""
+        location = soup.find("div", class_="title_area _area_panel").find('h2', class_="title").getText()
+        celsius = soup.find('div', class_='temperature_text').find('strong').getText()
+        response = f"""현재 {location}의 {celsius} 입니다."""
+        response = response.replace("현재 온도", "현재 온도는 ")
     except Exception as e:
         print(e)
         response = "해당 지역의 날씨정보를 불러올 수 없습니다."
 
+    
     await message.channel.send(response)
